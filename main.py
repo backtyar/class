1134c86f219b9aae30665020e3e4e27b69a2a591
backtyar <b11703597@gmail.com>
2023-10-10 23:17:58 +0600

    update_commit

--- main.py
+++ main.py
@@ -6,24 +6,24 @@
 # Затем выполните несколько операций по депозиту и снятию средств,
 # используя методы экземпляра, и выведите итоговый баланс.
 
-# class BankAccount:
-#     interest_rate = 10
-#
-#     def __init__(self, money, inner_interest_rate):
-#         self.money = money
-#         self.inner_interest_rate = inner_interest_rate
-#
-#     def deposit(self, deposit):
-#         self.money += deposit
-#
-#     def withdraw(self, spend):
-#         self.money -= spend
-#
-#
-# bank = BankAccount(1000,7)
-# bank.deposit(6000)
-# bank.withdraw(2500)
-# print(bank.money)
+class BankAccount:
+
+    def __init__(self, money, interest_rate):
+        self.money = (money + (money * (interest_rate/100)))
+
+    def deposit(self, deposit):
+        self.money += deposit
+
+    def withdraw(self, spend):
+        self.money -= spend
+
+
+bank = BankAccount(11500, 10)
+
+bank.deposit(10000)
+bank.withdraw(8000)
+print(bank.money)
+
 
 
 
@@ -31,21 +31,21 @@
 # width и height. Добавьте метод экземпляра calculate_area(),
 # который возвращает площадь прямоугольника (ширина * высота).
 # Создайте объект этого класса и вызовите метод calculate_area().
+print('*' * 50)
+class Rectangle:
 
-# class Rectangle:
-#
-#     def __init__(self, width=0, height=0):
-#         self.width = width
-#         self.height = height
-#
-#     def calculate_area(self):
-#         return self.width * self.height
-#
-#
-# rectangle_1 = Rectangle(5, 5)
-# rectangle_2 = Rectangle(8, 11)
-# print(rectangle_1.calculate_area())
-# print(rectangle_2.calculate_area())
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    def calculate_area(self):
+        return self.width * self.height
+
+
+rectangle_1 = Rectangle(5, 5)
+rectangle_2 = Rectangle(8, 11)
+print(rectangle_1.calculate_area())
+print(rectangle_2.calculate_area())
 
 
 
@@ -53,22 +53,23 @@
 # значением 0. Добавьте методы increment() и decrement(), которые увеличивают
 # и уменьшают значение атрибута count. Создайте объект этого класса и вызовите
 # методы для изменения значения count.
+print('*' * 50)
+class Counter:
 
-# class Counter:
-#
-#     def __init__(self):
-#         self.count = 0
-#
-#     def increment(self, num):
-#         self.count += num
-#
-#     def decrement(self, num):
-#         self.count -= num
-#
-#
-# counter = Counter()
-# print(counter.count)
-# counter.increment(5)
-# print(counter.count)
-# counter.decrement(1000)
-# print(counter.count)
+    def __init__(self):
+        self.count = 0
+
+    def increment(self, num):
+        self.count += num
+
+    def decrement(self, num):
+        self.count -= num
+
+
+counter = Counter()
+print(counter.count)
+counter.increment(5)
+print(counter.count)
+counter.decrement(1000)
+print(counter.count)
+
